23251fd1e65d1fa3dd485f80205cf7f22d6aa5fe
Yosief27 <josiyoniug@gmail.com>
2024-04-22 22:15:55 +0200

    add tailwindcss in game file

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 
 import './Game.css'
-
+const INITIAL_GAME_STATE=["X","O","X","O","X","O","X","O","X"];
 function Game() {
-
+  const [gameState,setGameState]=useState(INITIAL_GAME_STATE);
   return <>
           <div className='h-full p-8 text-slate-800 bg-gradient-to-br  from-cyan-500 to-gray-500'>
               <h1 className='text-center text-5xl mb-4 font-extrabold  text-white'>Tic Tac Toe</h1>
            <div>
-              <div> Board Goes Here! </div>
+              <div className='grid grid-cols-3 gap-3 mx-auto w-96'> 
+                {gameState.map((item,index)=>(
+                  <div key={index} className='h-36 border-solid border-4 border-slate-200 font-display text-7xl text-center flex justify-center items-center'>
+                    {item}
+                  </div>
+                ))}
+              </div>
               <div>Scores Go Here </div>
             </div>
         </div>
